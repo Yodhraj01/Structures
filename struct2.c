0a743aae2bc52292fca57ad9daf9c93e22c31a22
Yodhraj01 <ajay962778@gmail.com>
2026-04-04 09:30:09 +0530

    today work

--- struct2.c
+++ struct2.c
@@ -1,2 +1,36 @@
-//ajay
-wdc
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+    typedef struct pokemon{
+        char name[15];
+        int hp;
+        int speed;
+    } pokemon;
+
+    typedef struct legendarypokemon{
+        pokemon normal;
+        char ability[15];
+    } legendarypokemon;
+
+    typedef struct godpokemon{
+        legendarypokemon legend;
+        int specialattack;
+    } godpokemon;
+
+    godpokemon arceus;
+
+    arceus.specialattack = 300;
+    strcpy(arceus.legend.ability, "turn into stone");
+    arceus.legend.normal.hp = 500;
+    arceus.legend.normal.speed = 200;
+    strcpy(arceus.legend.normal.name, "Arceus");
+
+    printf("Name: %s\n", arceus.legend.normal.name);
+    printf("HP: %d\n", arceus.legend.normal.hp);
+    printf("Speed: %d\n", arceus.legend.normal.speed);
+    printf("Ability: %s\n", arceus.legend.ability);
+    printf("Special Attack: %d\n", arceus.specialattack);
+
+    return 0;
+}
